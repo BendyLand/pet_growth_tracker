5a336ffbd68a460ba88aaff20af464e7c533158f
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-15 12:28:12 -0400

    Initial commit

---
+++ queue.rb
@@ -0,0 +1,67 @@
+require './node.rb'
+
+class Queue
+    attr_reader :max_size
+    attr_accessor :head, :tail, :size
+
+    def initialize(max_size = nil)
+        @head = nil
+        @tail = nil
+        @max_size = max_size
+        @size = 0
+    end
+
+    def enqueue(value)
+        if self.has_space()
+            item_to_add = Node.new(value)
+            # puts "Adding \"#{item_to_add.value}\" to the queue!"
+            if self.is_empty()
+                @head = item_to_add
+                @tail = item_to_add
+            else
+                @tail.next_node = item_to_add
+                @tail = item_to_add
+            end
+            @size += 1
+        else
+            puts "Sorry, no more room!"
+        end
+    end
+
+    def dequeue()
+        if @size > 0
+            item_to_remove = @head
+            puts "#{item_to_remove.value} is served!"
+            if @size == 1
+                @head = nil
+                @tail = nil
+            else
+                @head = @head.next_node
+            end
+            @size -= 1
+            return item_to_remove.value
+        else
+            puts "The queue is totally empty!"
+        end
+    end
+
+    def peek()
+        if @size > 0
+            return @head.value
+        else
+            puts "No orders waiting."
+        end
+    end
+
+    def has_space()
+        if @max_size == nil
+            return true
+        else
+            return @max_size > @size
+        end
+    end
+
+    def is_empty()
+        @size == 0
+    end
+end
---
+++ script.rb
@@ -0,0 +1,47 @@
+require 'date'
+require './stack.rb'
+
+growth = Stack.new
+reese = Stack.new
+puff = Stack.new
+
+today = DateTime.now.strftime("%m/%d/%Y")
+
+information = "\n\nThe data is provided numerically for comparison.\nLength is in inches.\nWeight is in grams.\nAge is my closest approximation based on the initial data.\n\n\n"
+
+reese_initial_data = {
+    date: "09/07/23",
+    length: 7.5,
+    weight: 510,
+    age: 12
+}
+puff_initial_data = {
+    date: "09/07/23",
+    length: 8,
+    weight: 623,
+    age: 22
+}
+
+reese.push(reese_initial_data)
+puff.push(puff_initial_data)
+
+r_measurement_two = {
+    date: "09/14/23",
+    length: 7.75,
+    weight: 550,
+    age: 13
+}
+p_measurement_two = {
+    date: "09/14/23",
+    length: 8.63,
+    weight: 709,
+    age: 23
+}
+
+reese.push(r_measurement_two)
+puff.push(p_measurement_two)
+
+# print information
+growth.push({today: today, reese: reese, puff: puff})
+# usage growth.peek[:pet].peek[:attribute]
+puts growth.peek[:reese].peek
